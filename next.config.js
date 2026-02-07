567b2e97be94a19418b57fb8553ed2ce623d4bf4
Angel Aparicio <angel.aparicio92@gmail.com>
2026-02-06 23:09:37 -0500

    chore: setup next-intl in next.config.js and restore config

--- next.config.js
+++ next.config.js
@@ -1,29 +1,31 @@
+const withNextIntl = require('next-intl/plugin')();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    formats: ['image/avif', 'image/webp'],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'i.ibb.co',
-      },
-    ],
-  },
-  experimental: {
-    optimizePackageImports: ['framer-motion', 'three', '@react-three/fiber', '@react-three/drei'],
-  },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(glsl|vs|fs|vert|frag)$/,
-      exclude: /node_modules/,
-      use: ['raw-loader', 'glslify-loader'],
-    });
-    return config;
-  },
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+        formats: ['image/avif', 'image/webp'],
+        deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+        imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'i.ibb.co',
+            },
+        ],
+    },
+    experimental: {
+        optimizePackageImports: ['framer-motion', 'three', '@react-three/fiber', '@react-three/drei'],
+    },
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.(glsl|vs|fs|vert|frag)$/,
+            exclude: /node_modules/,
+            use: ['raw-loader', 'glslify-loader'],
+        });
+        return config;
+    },
 };
 
-module.exports = nextConfig;
+module.exports = withNextIntl(nextConfig);
